3b026c0a48bb209fd3c569b64745d8a13b0129cd
twentyseven <trongbuituan@gmail.com>
2021-02-01 19:50:53 +1030

    Update

--- PlainExtension/Sources/OSLog.swift
+++ PlainExtension/Sources/OSLog.swift
@@ -76,6 +76,7 @@ public extension OSLog {
 public extension OSLog {
     func logError(_ error: Error?) {
         guard let error = error else { return }
-        self.error("%@:%@:%@: Error fetching document: %@", ("\(#file)" as NSString).lastPathComponent, "\(#function)", "\(#line)", "\(String(describing: error))")
+//        self.error("%@:%@:%@: Error fetching document: %@", ("\(#file)" as NSString).lastPathComponent, "\(#function)", "\(#line)", "\(String(describing: error))")
+        self.error("Error: %@", "\(String(describing: error))")
     }
 }
