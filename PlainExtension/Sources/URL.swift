5ffe3404b59b82fb1d2172f021779bbfcd940272
twentyseven <trongbuituan@gmail.com>
2021-07-06 14:53:30 +0930

    update

---
+++ PlainExtension/Sources/URL.swift
@@ -0,0 +1,19 @@
+//
+//  URL.swift
+//  PlainExtension
+//
+//  Created by Trong Bui on 6/7/21.
+//
+
+import Foundation
+
+public extension URL {
+    var queryParameters: [String: String]? {
+        guard
+            let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+            let queryItems = components.queryItems else { return nil }
+        return queryItems.reduce(into: [String: String]()) { result, item in
+            result[item.name] = item.value
+        }
+    }
+}
